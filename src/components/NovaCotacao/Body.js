28cf36c954c67f9383dc666178f0e5da8d76ec78
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2018-10-20 10:54:57 -0300

    mais um pouco do layout

--- src/components/NovaCotacao/Body.js
+++ src/components/NovaCotacao/Body.js
@@ -1,34 +1,60 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import '../../App.css';
+
+import './NovaCotacao.css';
 
 import { Icon } from 'react-icons-kit';
 import { lineChart } from 'react-icons-kit/fa/lineChart';
 
 
-class NovaCotacao extends Component {
-    componentDidMount() {
-        document.body.classList.add('NovaCotacao-body');
+class Body extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { cnpj: '' }
+        this.handleChange = this.handleChange.bind(this);
     }
 
+    handleChange(e) {
+        this.state.cnpj = e.target.value;
+        console.log(this.state.cnpj);
+    }
     render() {
         return (
-            <ul className="NovaCotacao-body">
-                <li>
-                    <Icon icon={lineChart} />
-                    <div>
-                        <p className="title">Busca por CNPJ ou empresa</p>
-                    </div>
-                </li>
-                <li>
-                    <div></div>
-                    <div>
-                        <input />
-                    </div>
-                </li>
-            </ul>
+            <div>
+                <ul className="NovaCotacao-body">
+                    <li>
+                        <Icon icon={lineChart} />
+                        <div>
+                            <p className="title">Busca por CNPJ ou empresa</p>
+                        </div>
+                    </li>
+                </ul>
+                <ul className="NovaCotacao-body NovaCotacao-body-input">
+                    <li>
+                        <div></div>
+                        <div>
+                            <p className="title">
+                                <small>CNPJ/Empresa</small>
+                            </p>
+                            <input required="required"
+                                onChange={this.handleChange}
+                                pattern="^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$"
+                                value={this.state.cnpj} />
+                        </div>
+                    </li>
+                </ul>
+            </div>
         );
     }
 }
 
-export default NovaCotacao;
+Body.propTypes = {
+    cnpj: PropTypes.string
+};
+
+Body.defaultProps = {
+    cnpj: ''
+};
+
+export default Body;
